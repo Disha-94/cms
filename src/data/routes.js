af571235da983ee8ca21b61593ccbd95f31fa49f
seagel <mehta.somesh636@gmail.com>
2022-05-19 16:16:52 -0400

    added everything

---
+++ src/data/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    index: true,
+    label: 'Carbon Monitoring System',
+    path: '/',
+  },
+  {
+    label: 'About',
+    path: '/about',
+  },
+  {
+    label: 'Project Sites',
+    path: '/resume',
+  },
+  {
+    label: 'Project Updates',
+    path: '/projects',
+  },
+  {
+    label: 'Stats',
+    path: '/stats',
+  },
+  {
+    label: 'Resources',
+    path: '/contact',
+  },
+];
+
+export default routes;
